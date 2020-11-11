ffc3dd22a601eea129ccd6e56940e68b5732fe84
Bima <68883585+ZalEmperon@users.noreply.github.com>
2020-11-11 12:09:21 +0700

    Update get.php

--- get.php
+++ get.php
@@ -6,7 +6,7 @@
 <body>
 	<fieldset id="ini">
 		<label for="ini">DATA</label>
-		<form method="GET" action="proces.php">
+		<form method="POST" action="proces.php">
 			<p>Nama : <input type="text" name="nama"></p>
 			<p>Alamat : <input type="text" name="alamat"></p>
 			<p><input type="submit" value="Proses" name="submit"></p>
