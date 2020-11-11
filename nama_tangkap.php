cd1df1d72dbd2b919540b951afcb7c05a719818c
Bima <68883585+ZalEmperon@users.noreply.github.com>
2020-11-11 12:35:08 +0700

    PHP

--- nama_tangkap.php
+++ nama_tangkap.php
@@ -3,14 +3,14 @@
 <head>
 	<title>Get</title>
 </head>
-<body>
-	<fieldset id="ini">
-		<label for="ini">DATA</label>
-		<form method="POST" action="proses.php">
-			<p>Nama : <input type="text" name="nama"></p>
-			<p>Alamat : <input type="text" name="alamat"></p>
-			<p><input type="submit" value="Proses" name="submit"></p>
-		</form>
-	</fieldset>
-</body>
+	<body>
+		<fieldset id="ini">
+			<label for="ini">DATA</label>
+			<form method="POST" action="proses.php">
+				<p>Nama : <input type="text" name="nama"></p>
+				<p>Alamat : <input type="text" name="alamat"></p>
+				<p><input type="submit" value="Proses" name="submit"></p>
+			</form>
+		</fieldset>
+	</body>
 </html>
